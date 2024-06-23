01b1015c05c9302b8ec95392a34d487ed5e62405
arsyadthareeq <thareq.darmawan@amarbank.co.id>
2024-06-23 16:14:56 +0700

    feat: add github action and rest api

--- internal/app/book/usecase/book.go
+++
@@ -1,8 +0,0 @@
-package usecase
-
-import "context"
-
-type Book interface {
-	Get(ctx context.Context) []string
-	GetByID(ctx context.Context, id string) string
-}
---
+++ model.go
@@ -0,0 +1,99 @@
+package main
+
+type Book struct {
+	ID               string `json:"id"`
+	Title            string `json:"title"`
+	Author           string `json:"author"`
+	PublishedDate    string `json:"published_date"`
+	OriginalLanguage string `json:"original_language"`
+}
+
+var books = []*Book{
+	{
+		ID:               "1",
+		Title:            "7 habits of Highly Effective People",
+		Author:           "Stephen Covey",
+		PublishedDate:    "15/08/1989",
+		OriginalLanguage: "English",
+	},
+}
+
+type BookStorage interface {
+	List() []*Book
+	Get(string) *Book
+	Update(string, Book) *Book
+	Create(Book)
+	Delete(string) *Book
+}
+
+type BookStore struct {
+}
+
+func (b BookStore) List() []*Book {
+	return books
+}
+
+func (b BookStore) Get(id string) *Book {
+	for _, book := range books {
+		if book.ID == id {
+			return book
+		}
+	}
+	return nil
+}
+
+func (b BookStore) Update(s string, bookUpdate Book) *Book {
+	for i, book := range books {
+		if book.ID == s {
+			books[i] = &bookUpdate
+			return book
+		}
+	}
+	return nil
+}
+
+func (b BookStore) Create(book Book) {
+	books = append(books, &book)
+}
+
+func (b BookStore) Delete(s string) *Book {
+	//TODO implement me
+	panic("implement me")
+}
+
+func listBooks() []*Book {
+	return books
+}
+
+func getBook(id string) *Book {
+	for _, book := range books {
+		if book.ID == id {
+			return book
+		}
+	}
+	return nil
+}
+
+func storeBook(book Book) {
+	books = append(books, &book)
+}
+
+func deleteBook(id string) *Book {
+	for i, book := range books {
+		if book.ID == id {
+			books = append(books[:i], (books)[i+1:]...)
+			return &Book{}
+		}
+	}
+	return nil
+}
+
+func updateBook(id string, bookUpdate Book) *Book {
+	for i, book := range books {
+		if book.ID == id {
+			books[i] = &bookUpdate
+			return book
+		}
+	}
+	return nil
+}
